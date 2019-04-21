2ed03660b99fba5727dda638dfcc1c62724f6181
jamessouth <jamesdanielsouth@gmail.com>
2019-04-21 00:48:04 -0500

    add msg btw popup and background

--- src/background.js
+++ src/background.js
@@ -1,22 +1,51 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'}})
-      ],
-      actions: [
-        new chrome.declarativeContent.ShowPageAction()
-      ]
-    }]);
-  });
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({color: '#3aa757'}, function() {
+//     console.log('The color is green.');
+//   });
+// });
+
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+
+    // debugger;
+    // alert(anyWin.width);
+    chrome.windows.update(msg.win.id, {
+      state: 'maximized'
+    }, function(maxWin) {
+      // alert(maxWin.width);
+      const frameWidth = (maxWin.width - screen.availWidth) / 2;
+      // alert(frameWidth);
+      const obj = {
+        left: -frameWidth,
+        top: 0,
+        width: (screen.availWidth / 2) + (2 * frameWidth),
+        height: screen.availHeight + frameWidth
+      };
+      // alert(obj.width);
+      chrome.windows.update(maxWin.id, obj);
+
+    });
+
 });
 
 
-"background": {
-  "scripts": ["background.js"],
-  "persistent": false
-},
+
+
+// chrome.runtime.onInstalled.addListener(function() {
+
+// });
+
+// chrome.browserAction.onClicked.addListener(function (e) {
+//
+//   chrome.windows.getCurrent(function(w) {
+//     const resizeHandleSize = (w.width - window.innerWidth) / 2;
+//     alert(resizeHandleSize);
+//       // chrome.windows.update(w.id, {
+//       //   left: 0,
+//       //   top: 0,
+//       //   width: screen.width / 2,
+//       //   height: screen.height
+//       // });
+//   });
+//
+// });
--- src/popup.js
+++ src/popup.js
@@ -16,5 +16,7 @@
 const button = document.querySelector('button');
 
 button.addEventListener('click', function (e) {
-
+  chrome.windows.getCurrent(function(thisWin) {
+    chrome.runtime.sendMessage({win: thisWin});
+  });
 });
