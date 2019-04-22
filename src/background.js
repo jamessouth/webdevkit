e9a74423fc3876f7643068453d1b8a07a45c3989
jamessouth <jamesdanielsouth@gmail.com>
2019-04-22 01:56:59 -0500

    add storage to memoize frame size

--- src/background.js
+++ src/background.js
@@ -1,51 +1,37 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.storage.sync.set({color: '#3aa757'}, function() {
-//     console.log('The color is green.');
-//   });
-// });
+// chrome.storage.local.clear();
+
+
+function getSizeObj(fW) {
+  return {
+    left: -fW,
+    top: 0,
+    width: (screen.availWidth / 2) + (2 * fW),
+    height: screen.availHeight + fW
+  };
+};
+
+
+const maxObj = {
+  state: 'maximized'
+};
 
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
-    // debugger;
-    // alert(anyWin.width);
-    chrome.windows.update(msg.win.id, {
-      state: 'maximized'
-    }, function(maxWin) {
-      // alert(maxWin.width);
-      const frameWidth = (maxWin.width - screen.availWidth) / 2;
-      // alert(frameWidth);
-      const obj = {
-        left: -frameWidth,
-        top: 0,
-        width: (screen.availWidth / 2) + (2 * frameWidth),
-        height: screen.availHeight + frameWidth
-      };
-      // alert(obj.width);
-      chrome.windows.update(maxWin.id, obj);
-
-    });
+  chrome.storage.local.get('frame', function(data) {
+    console.log(data.frame);
+    if (data.frame) {
+      console.log('frame known');
+      chrome.windows.update(msg.win.id, getSizeObj(data.frame));
+    } else {
+      console.log('frame unknown');
+      chrome.windows.update(msg.win.id, maxObj, function(maxWin) {
+        const frameWidth = (maxWin.width - screen.availWidth) / 2;
+        chrome.storage.local.set({frame: frameWidth}, function() {
+          chrome.windows.update(maxWin.id, getSizeObj(frameWidth));
+        });
+      });
+    }
+  });
 
 });
-
-
-
-
-// chrome.runtime.onInstalled.addListener(function() {
-
-// });
-
-// chrome.browserAction.onClicked.addListener(function (e) {
-//
-//   chrome.windows.getCurrent(function(w) {
-//     const resizeHandleSize = (w.width - window.innerWidth) / 2;
-//     alert(resizeHandleSize);
-//       // chrome.windows.update(w.id, {
-//       //   left: 0,
-//       //   top: 0,
-//       //   width: screen.width / 2,
-//       //   height: screen.height
-//       // });
-//   });
-//
-// });
