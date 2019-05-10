90712b034f0ea6170301aeb7ccca9f104d7c8fce
jamessouth <jamesdanielsouth@gmail.com>
2019-05-10 13:48:09 -0500

    add badge, fix popup styles

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,14 @@
 const buttons = document.querySelectorAll('button');
+const os = navigator.userAgent.includes('Windows') ? 'Win' :
+           navigator.userAgent.includes('Mac') ? 'Mac' :
+           'Linux';
 
 for (b of buttons) {
   b.addEventListener('click', function (e) {
     chrome.windows.getCurrent(function(thisWin) {
       chrome.runtime.sendMessage({
         win: thisWin,
+        os: os,
         edge: e.target.dataset.edge,
         size: e.target.dataset.size
       });
